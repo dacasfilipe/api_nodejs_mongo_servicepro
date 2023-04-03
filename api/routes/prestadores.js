c36d4cbd3b5c7e6d3adab935e9ce14219956d2ca
dacasfilipe <filiperdacas@gmail.com>
2023-04-03 00:21:03 -0300

    atualizado as rotas

--- api/routes/prestadores.js
+++ api/routes/prestadores.js
@@ -4,8 +4,17 @@ const express = require('express');
 const router = express.Router();
 const prestadoresController = require('../controllers/prestadoresController');
 
+// Criar um prestador de serviços
 router.post('/', prestadoresController.createPrestador);
-router.get('/', prestadoresController.getPrestadores); // Adicione esta linha
+
+// Listar todos os prestadores de serviços
+router.get('/', prestadoresController.getPrestadores);
+
+// Atualizar um prestador de serviços pelo ID
+router.put('/:id', prestadoresController.updatePrestador);
+
+// Excluir um prestador de serviços pelo ID
+router.delete('/:id', prestadoresController.deletePrestador);
 
 module.exports = router;
 
